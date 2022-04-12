9908af6ee9e399025bff7de6987fb1690ae72a67
PrashanthgRebel <92351464+PrashanthgRebel@users.noreply.github.com>
2022-04-13 01:13:17 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -3,21 +3,5 @@
 echo "hello world"
 
 ls 
+echo "my name is prashanthgr"
 
-while ( true );do
-
-	echo "checking count of dbteam_temp.tbl_paid_mobile"
-	#count=`sudo mysql  --socket=/var/lib/SQL_07/mysql/mysql.sock --port=3307 -Bse "SELECT COUNT(1) FROM dbteam_temp.tbl_paid_mobile WHERE is_done = 0;"`
-	#echo "count is = $count"
- 
-	if [ $? -eq 0 ]
-	then
-		
-		echo "$count is <= 20 , proceeding to step=8"
-
-	else
-		
-		echo "count is greater then 20 so sleeping for 1 hour"
-        sleep 3600
-	fi
-done
